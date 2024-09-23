12be72a7f610375208d82874bbee89f95deb87ae
gabitoeo <gebelen@udc.edu.ar>
2024-09-23 18:41:25 -0300

    views, templates, urls.py

--- blog_project/Blog/post/views.py
+++ blog_project/Blog/post/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView, DetailView, DeleteView, UpdateView
 from .models import Post
 
 class PostCreateView(CreateView):
@@ -12,3 +12,32 @@ class PostCreateView(CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post/post_list.html'
+    context_object_name = 'posts'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post/post_detail.html'
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'post/post_form.html'
+    success_url = reverse_lazy('post-list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+    
