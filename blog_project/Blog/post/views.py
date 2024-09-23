2871b0a2627024fe425a81ce3d7820e857287704
gabitoeo <gebelen@udc.edu.ar>
2024-09-23 18:26:20 -0300

    tasks

--- blog_project/Blog/post/views.py
+++ blog_project/Blog/post/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from .models import Post
 
-# Create your views here.
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'post/post_form.html'
+    success_url = reverse_lazy('post-list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
