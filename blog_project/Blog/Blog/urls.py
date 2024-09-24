1f72345736987c187b3399a54e350018d8cdc360
gabitoeo <gebelen@udc.edu.ar>
2024-09-24 17:36:17 -0300

    done?

--- blog_project/Blog/Blog/urls.py
+++ blog_project/Blog/Blog/urls.py
@@ -6,8 +6,8 @@ from post import views as post_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='post/login.html'), name='login'),
-    path('', auth_views.LoginView.as_view(template_name='post/login.html'), name='login'),  # Página principal es login
+    path('', auth_views.LoginView.as_view(template_name='post/login.html'), name='login'), 
     path('register/', post_views.register, name='register'),
-    path('posts/', include('post.urls')),  # Las URLs de los posteos van aquí
+    path('posts/', include('post.urls')), 
     path('logout/', auth_views.LogoutView.as_view(template_name='post/login.html'), name='logout'),
 ]
